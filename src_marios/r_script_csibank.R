a13a4edf3d90c0000f6c04cb2aca3acf379c4f3b
mamxlam <mamxlam@teiemt.gr>
2024-01-14 17:28:57 +0200

    Added t-tests and corr tests along with scatterplots for finding correlations.

--- src_marios/r_script_csibank.R
+++ src_marios/r_script_csibank.R
@@ -7,10 +7,10 @@ library(summarytools)
 library(psych)
 
 # install impute package
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("impute")
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#
+#BiocManager::install("impute")
 
 
 # General information about dataframe
@@ -142,15 +142,21 @@ for (col in real_columns) {
 # ====================================================================================
 # Missing values imputation
 
-library(impute)
+#library(impute)
 # Identify columns with a suitable data type for k-NN imputation (e.g., numeric columns)
-numeric_columns <- sapply(csibank, is.numeric)
+#numeric_columns <- sapply(csibank, is.numeric)
 
 # Apply k-NN imputation only to numeric columns
-impute_model <- impute.knn(csibank[, numeric_columns])
+#impute_model <- impute.knn(csibank[, numeric_columns], k=3)
 
 # Impute missing values in the original dataframe
-csibank[, numeric_columns] <- complete(impute_model)
+#csibank[, numeric_columns] <- complete(impute_model)
+
+
+library(tidyverse)
+csibank_imputed <- csibank %>% 
+  mutate(across(where(~!is.factor(.)), ~ifelse(is.na(.), median(., na.rm = TRUE), .)))
+
 
 
 # ====================================================================================
@@ -161,7 +167,342 @@ csibank[, numeric_columns] <- complete(impute_model)
 detach("package:psych", unload = TRUE)
 library(car)
 
+# Perform tranformation of dependent variable
+log_loy <- log(csibank$loy_avg)
+log_loy[log_loy == -Inf] <- NA
 
-leveneTest(log(loy_avg) ~ Age,
+
+subset_data <- subset(csibank, Age %in% c("<=25", "36-45"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Age,
           center=mean,
-          data=csibank)
+          data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#        Df F value Pr(>F)
+# group   1  0.2664  0.606
+#      507   
+
+t.test(log_loy_sub ~ Age,
+       data=subset_data,
+       var.equal=T)
+
+# Two Sample t-test
+# 
+# data:  log_loy_sub by Age
+# t = 0.93665, df = 507, p-value = 0.3494
+# alternative hypothesis: true difference in means between group <=25 and group 36-45 is not equal to 0
+# 95 percent confidence interval:
+#   -0.06360923  0.17952222
+# sample estimates:
+#   mean in group <=25 mean in group 36-45 
+# 1.916506            1.858549 
+
+# -------------------------------------------------------
+
+# perform test for homogeneity of variance
+leveneTest(log_loy ~ Gender,
+           center=mean,
+           data=csibank)
+# Levene's Test for Homogeneity of Variance (center = mean)
+#         Df F value Pr(>F)
+# group    1  1.0484  0.306
+
+
+# variance hypothesis is satisfied. Proceeding with t-test
+t.test(log_loy ~ Gender,
+       data=csibank,
+       var.equal=T)
+
+
+# Two Sample t-test
+# 
+# data:  log_loy by Gender
+# t = -0.016037, df = 1673, p-value = 0.9872
+# alternative hypothesis: true difference in means between group Female and group Male is not equal to 0
+# 95 percent confidence interval:
+#   -0.04160523  0.04093040
+# sample estimates:
+#   mean in group Female   mean in group Male 
+# 1.884426             1.884764 
+
+# -------------------------------------------------------
+
+
+
+subset_data <- subset(csibank, Region %in% c("East", "North"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Region,
+           center=mean,
+           data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#         Df F value Pr(>F)
+# group    1  1.2011 0.2733
+#       1076           
+
+t.test(log_loy_sub ~ Region,
+       data=subset_data,
+       var.equal=T)
+
+# Two Sample t-test
+# 
+# data:  log_loy_sub by Region
+# t = 0.91154, df = 1076, p-value = 0.3622
+# alternative hypothesis: true difference in means between group East and group North is not equal to 0
+# 95 percent confidence interval:
+#   -0.02677549  0.07323725
+# sample estimates:
+#   mean in group East mean in group North 
+# 1.913579            1.890348 
+
+
+
+# -------------------------------------------------------
+
+# T-test for Manager - MediumEmploy in relation to loyalty
+
+subset_data <- subset(csibank, Occupation %in% c("Manager", "MediumEmplo"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Occupation,
+           center=mean,
+           data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#         Df F value Pr(>F)
+# group    1  1.2011 0.2733
+#       1076           
+
+t.test(log_loy_sub ~ Occupation,
+       data=subset_data,
+       var.equal=T)
+
+# Two Sample t-test
+# 
+# data:  log_loy_sub by Occupation
+# t = -1.4676, df = 810, p-value = 0.1426
+# alternative hypothesis: true difference in means between group Manager and group MediumEmplo is not equal to 0
+# 95 percent confidence interval:
+#   -0.13431523  0.01939391
+# sample estimates:
+#   mean in group Manager mean in group MediumEmplo 
+# 1.809779                  1.867239 
+
+
+# -------------------------------------------------------
+
+# T-test for Manager - MediumEmploy in relation to loyalty
+
+subset_data <- subset(csibank, Occupation %in% c("Manager", "Retired"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Occupation,
+           center=mean,
+           data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#        Df F value  Pr(>F)  
+# group   1  4.9333 0.02684 *
+#       456                  
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1      
+
+
+t.test(log_loy_sub ~ Occupation,
+       data=subset_data,
+       var.equal=F)
+
+# Welch Two Sample t-test
+# 
+# data:  log_loy_sub by Occupation
+# t = -2.4179, df = 312.56, p-value = 0.01618
+# alternative hypothesis: true difference in means between group Manager and group Retired is not equal to 0
+# 95 percent confidence interval:
+#   -0.20089605 -0.02062864
+# sample estimates:
+#   mean in group Manager mean in group Retired 
+# 1.809779              1.920541
+
+
+# Statistical significant difference between managers and retired over loyalty towards the bank.
+
+
+
+# -------------------------------------------------------
+
+# T-test for Unfinished - Elementary in relation to loyalty
+
+subset_data <- subset(csibank, Education %in% c("Unfinished", "Elementary"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Education,
+           center=mean,
+           data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#        Df F value Pr(>F)
+# group   1  0.0565 0.8123
+#       385       
+
+
+t.test(log_loy_sub ~ Education,
+       data=subset_data,
+       var.equal=T)
+
+# Two Sample t-test
+# 
+# data:  log_loy_sub by Education
+# t = -0.67512, df = 385, p-value = 0.5
+# alternative hypothesis: true difference in means between group Unfinished and group Elementary is not equal to 0
+# 95 percent confidence interval:
+#   -0.18105371  0.08849767
+# sample estimates:
+#   mean in group Unfinished mean in group Elementary 
+# 1.894819                 1.941097
+
+
+# No Statisticaly significant difference between Unfinished and Elementary over loyalty towards the bank.
+
+
+# -------------------------------------------------------
+
+# T-test for Unfinished - Graduated in relation to loyalty
+
+subset_data <- subset(csibank, Education %in% c("Unfinished", "Graduated"))
+log_loy_sub <- log(subset_data$loy_avg)
+log_loy_sub[log_loy_sub == -Inf] <- NA
+
+# perform test for homogeneity of variance
+leveneTest(log_loy_sub ~ Education,
+           center=mean,
+           data=subset_data)
+
+# Levene's Test for Homogeneity of Variance (center = mean)
+#        Df F value Pr(>F)
+# group   1   1.769  0.186
+#       121  
+
+
+t.test(log_loy_sub ~ Education,
+       data=subset_data,
+       var.equal=T)
+
+# Two Sample t-test
+# 
+# data:  log_loy_sub by Education
+# t = 1.2778, df = 121, p-value = 0.2038
+# alternative hypothesis: true difference in means between group Unfinished and group Graduated is not equal to 0
+# 95 percent confidence interval:
+#   -0.07108186  0.32984140
+# sample estimates:
+#   mean in group Unfinished  mean in group Graduated 
+# 1.894819                 1.765440 
+
+
+# No Statistically significant difference between Unfinished and Graduated over loyalty towards the bank.
+
+# -------------------------------------------------------
+
+library(GGally)
+
+selected_columns <- csibank %>% select(ends_with("_avg"))
+selected_columns <- log(selected_columns)
+selected_columns[selected_columns == -Inf] <- NA
+
+ggpairs(
+  selected_columns,
+  upper = list(
+    continuous = wrap("cor", method = "pearson", size = 5) # As we use log than we can use pearson (parametric)
+  ),
+  diag = list(
+    continuous = wrap("densityDiag", alpha = 0.5)
+  ),
+  lower = list(
+    continuous = wrap("points", size = 1, alpha = 0.8)
+  )
+) +
+  theme_bw() +
+  theme(
+    legend.position = "none",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 14, face = "bold"),
+    strip.text.x = element_text(size = 12, hjust = 0.5, vjust = 0.5, face = 'bold'),
+    strip.text.y = element_text(size = 6, hjust = 0.5, vjust = 0.5, face = 'bold'),
+    axis.text.x = element_text(face = "bold", color = "black", size = 14),
+    axis.text.y = element_text(face = "bold", color = "black", size = 14),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(face = "bold", color = "black", size = 20),
+    plot.title = element_text(face = "bold", color = "black", size = 22, hjust = 0.5),
+    panel.grid = element_blank())
+
+
+# -------------------------------------------------------
+
+# From histograms, imag_avg and reputation do not follow normal dist
+calculate_pears_correlation <- function(df1, x_column, y_column) {
+  # Log transformation and handling -Inf values for df1
+  log_y_df1 <- log(df1[[y_column]])
+  log_y_df1[is.infinite(log_y_df1)] <- NA
+  
+  log_x_df1 <- log(df1[[x_column]])
+  log_x_df1[is.infinite(log_x_df1)] <- NA
+  
+  
+  # Correlation test
+  correlation_result <- cor.test(log_y_df1, log_x_df1, method = "pearson")
+  
+  # Print or return the result
+  print(correlation_result)
+  # Alternatively, you can return the result if you want to store or further process it.
+  # return(correlation_result)
+}
+
+
+# From histograms, imag_avg and reputation do not follow normal dist
+calculate_spear_correlation <- function(df1, x_column, y_column) {
+  # Correlation test
+  correlation_result <- cor.test(df1[[y_column]],df1[[x_column]], method = "spearman")
+  
+  # Print or return the result
+  print(correlation_result)
+  # Alternatively, you can return the result if you want to store or further process it.
+  # return(correlation_result)
+}
+
+calculate_pears_correlation(csibank, "imag_avg", "reputation") # p-value < 2.2e-16 , 0.809899
+calculate_pears_correlation(csibank, "imag_avg", "trustworthiness") # p-value < 2.2e-16 , 0.7882289
+calculate_pears_correlation(csibank, "imag_avg", "seriousness") # p-value < 2.2e-16 , 0.7662788  
+calculate_pears_correlation(csibank, "imag_avg", "solidness") # p-value < 2.2e-16 , 0.7559177
+calculate_pears_correlation(csibank, "imag_avg", "caring") # p-value < 2.2e-16 , 0.613812
+
+calculate_pears_correlation(csibank, "expe_avg", "prod_services") 
+calculate_pears_correlation(csibank, "expe_avg", "cust_services") 
+calculate_pears_correlation(csibank, "expe_avg", "solutions") 
+calculate_pears_correlation(csibank, "expe_avg", "expectations_qual")
+
+calculate_pears_correlation(csibank, "qual_avg", "reliable_services") 
+calculate_pears_correlation(csibank, "qual_avg", "range_products") 
+calculate_pears_correlation(csibank, "qual_avg", "pers_advice") 
+calculate_pears_correlation(csibank, "qual_avg", "overall_qual")
+
+calculate_pears_correlation(csibank, "val_avg", "beneficial_services") 
+calculate_pears_correlation(csibank, "val_avg", "valuable_investments") 
+calculate_pears_correlation(csibank, "val_avg", "quality_to_price") 
+calculate_pears_correlation(csibank, "val_avg", "price_to_quality")
+
+calculate_pears_correlation(csibank, "sat_avg", "fulfilled_expectations") 
+calculate_pears_correlation(csibank, "sat_avg", "satisf_against_other_banks") 
+calculate_pears_correlation(csibank, "sat_avg", "perf_against_ideal_bank") 
